a054c9f666169b2a76e7db8c374cf5d2642e8416
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2024-12-09 14:21:18 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,23 +1,46 @@
 import json, os, requests, bs4, getpass, subprocess, time
 
-log = []
 LOG_GUI = []
 LOG_OPERATOR = 0
-LOG_DATA= []
-LOG_DASH = []
+LOG_DATA = []
+LOG_CR = []
+LOG_BONUS = 0
 
 class data:
     name_98 = "98_OperatorAggregates.xml"
     dash = "dash.json"
-
+    bonus = "agree.xml"
+    FP = {
+        "База":180,
+        "Дисконт":150,
+        "Повышенный":210,
+        "Без ФЗ":50
+    }
+    TP = {
+        "0.00":0,
+        "1999.00":40,
+        "3999.00":60,
+        "4999.00":100,
+        "5999.00":120,
+        "7999.00":140,
+        "9999.00":150,
+        "14999.00":200,
+        "19999.00":250,
+        "24999.00":450,
+        "29999.00":500,
+        "39999.00":600,
+        "49999.00":700,
+        "59999.00":750,
+        "69999.00":800
+    }
 # Класс с основой системой подрузки данных + подсчёт
 class TruAR(data):
     username = getpass.getuser()
     #username = "volkovva"
     def __init__(self):
-        #self.calc("volkovva")
+        self.calc("volkovva")
         #self.calc(self.username)
-        self.get_datas("volkovva", "dash")
+        #self.get_datas("volkovva", "agreements")
     def _open_report(self, report):
         def find(file, path) -> bool:
             try:
@@ -25,7 +48,7 @@ class TruAR(data):
                 if file in _files : return True
                 else : return False
             except FileNotFoundError:
-                log.append('File Not Found')
+                #log.append('File Not Found')
                 return False
         match report:
             case "98":
@@ -42,11 +65,18 @@ class TruAR(data):
                 else:
                     os.system("""powershell -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('FileNotFound in path %s', 'TruAR', 'OK', 'Information')}""""" % ("C:\\Users\\%s\\Desktop\\Auto\\" % (self.username) + super().dash))
                     os._exit(0)
+            case "agreements":
+                if find(super().bonus, "C:\\Users\\%s\\Desktop\\Auto\\" % (self.username)) == True:
+                    with open("C:\\Users\\%s\\Desktop\\Auto\\%s" % (self.username, super().bonus), "r", encoding="utf-8") as file : get_data = file.read()
+                    return get_data
+                else:
+                    os.system("""powershell -Command "& {[System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms'); [System.Windows.Forms.MessageBox]::Show('FileNotFound in path %s', 'TruAR', 'OK', 'Information')}""""" % ("C:\\Users\\%s\\Desktop\\Auto\\" % (self.username) + super().bonus))
+                    os._exit(0)
     def get_datas(self, get_group, report):
         global LOG_OPERATOR
         global LOG_DATA
         get_data = self._open_report(report)
-        if report == "98" : data = bs4.BeautifulSoup(get_data, 'xml')
+        if report == "98" or report == "agreements" : data = bs4.BeautifulSoup(get_data, 'xml')
         elif report == "dash" : data = get_data
         with open("groups.json", "r", encoding="utf-8") as file_groups : data_group = json.load(file_groups)
         group = data_group["%s" % (get_group)]
@@ -60,6 +90,18 @@ class TruAR(data):
                     ))
                     LOG_OPERATOR = LOG_OPERATOR + 1
                 except AttributeError : print('Сотрудник: %s не найден в отчёте!\n-------' % (operator["name"]))
+            elif report == "agreements":
+                search = data.find_all("Details3", attrs={"Сотрудник_выдачи2": operator["name"]})
+                for info in search:
+                    try:
+                        #free_money = "ФЗ: Есть" if int(info.get("Отказ_от_ФЗ")) > 0 else "ФЗ: Отказ"
+                        free_money = info.get("Тип_ФЗ2")
+                        print("Сотрудник: %s --- Сумма: %s --- %s --- Клиент: %s --- ФЗ: %s --- ТП: %s" % (
+                            operator["name"], info.get("Сумма_кредита2"), free_money, info.get("ФИО_Клиента3"), 
+                            info.get("comis_insur_sum2"), info.get("comis_gold_card2")
+                        ))
+                        LOG_OPERATOR = LOG_OPERATOR + 1
+                    except AttributeError : print('Сотрудник: %s не найден в отчёте!\n-------' % (operator["name"]))
             elif report == "dash":
                 try:
                     for search in data:
@@ -75,6 +117,8 @@ class TruAR(data):
     def calc(self, get_group):
         global LOG_OPERATOR
         global LOG_DATA
+        global LOG_BONUS
+        global LOG_CR
         report_98 = self._open_report("98")
         data = bs4.BeautifulSoup(report_98, 'xml')
         with open("groups.json", "r", encoding="utf-8") as file_groups : data_group = json.load(file_groups)
@@ -102,11 +146,31 @@ class TruAR(data):
                     "CR SIGN": str(CR_SIGN)[:5]+"%"
                 }
                 print(_operator)
-                LOG_DATA.append(_operator)
+                LOG_CR.append(_operator)
                 LOG_OPERATOR = LOG_OPERATOR + 1
             except AttributeError : print('Сотрудник: %s не найден в отчёте!\n-------' % (operator["name"]))
         print('Кол-во сотрудников в отчёте: %s' % (LOG_OPERATOR))
-
+        time.sleep(2)
+        del report_98, data, file_groups, data_group, group
+        report_bonus = self._open_report("agreements")
+        data = bs4.BeautifulSoup(report_bonus, 'xml')
+        with open("groups.json", "r", encoding="utf-8") as file_groups : data_group = json.load(file_groups)
+        group = data_group["%s" % (get_group)]
+        for operator in group:
+            search = data.find_all("Details3", attrs={"Сотрудник_выдачи2": operator["name"]})
+            for info in search:
+                try:
+                    LOG_DATA[0]
+                except IndexError:
+                    LOG_DATA.append(operator["name"])
+                try:
+                    if operator["name"] == LOG_DATA[0]:
+                        LOG_BONUS = LOG_BONUS + super().FP[info.get("Тип_ФЗ2")] + super().TP[str(info.get("comis_gold_card2"))]
+                    else:
+                        print("Премия сотрудник\n\tСотрудник: %s\n\tПремия: %s" % (LOG_DATA[0], LOG_BONUS))
+                        LOG_DATA.clear()
+                        LOG_BONUS = 0
+                except AttributeError : pass
 
 if __name__ == "__main__":
     TruAR()
